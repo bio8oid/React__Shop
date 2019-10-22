076708e2983b989bf0bff5f5f39c8f0888993c9d
bio8oid <8bollod8@mail.co.uk>
2019-10-22 21:50:04 +0200

    Refactoring selector component

--- src/components/Selector/Selector.js
+++ src/components/Selector/Selector.js
@@ -1,56 +1,103 @@
 import React from "react";
-import { connect } from 'react-redux';
-import * as actions from '../../actions/actions';
-import { NavLink } from 'react-router-dom';
+import { connect } from "react-redux";
+import * as actions from "../../actions/actions";
+import { NavLink } from "react-router-dom";
 import "./Selector.scss";
 
 class Selector extends React.Component {
+  handleSort = e => {
+    const id = e.target.id;
 
-  handleSort = (e) => {
-    const id = e.target.id
+    if (id === "PriceAsc") {
+      this.props.sortedByPriceAsc();
+    }
+    if (id === "PriceDesc") {
+      this.props.sortedByPriceDesc();
+    }
+    if (id === "NameAsc") {
+      this.props.sortedByNameAsc();
+    }
+    if (id === "NameDesc") {
+      this.props.sortedByNameDesc();
+    }
+    if (id === "resetFilters") {
+      this.reset();
+    }
+    return false;
+  };
 
-    return (
-      (id === 'PriceAsc') ? this.props.sortedByPriceAsc() :
-        (id === 'PriceDesc') ? this.props.sortedByPriceDesc() :
-          (id === 'NameAsc') ? this.props.sortedByNameAsc() :
-            (id === 'NameDesc') ? this.props.sortedByNameDesc() :
-              (id === 'resetFilters') ? this.reset() : false
-    )
-  }
+  handleFilter = e => {
+    const id = e.target.id;
+    const checked = e.target.checked;
 
-  handleFilter = (e) => {
-    const id = e.target.id
-    const checked = e.target.checked
-
-    if (id === "risers") { (checked) ? this.props.filteredByTypeRisers() : this.props.risersRemoved(); };
-    if (id === "limbs") { (checked) ? this.props.filteredByTypeLimbs() : this.props.limbsRemoved(); };
-    if (id === "finger tabs") { (checked) ? this.props.filteredByTypeTabs() : this.props.tabsRemoved(); }
-    if (id === "stabilizers") { (checked) ? this.props.filteredByTypeStabilizers() : this.props.stabilizersRemoved(); }
-    if (id === "arrows") { (checked) ? this.props.filteredByTypeArrows() : this.props.arrowsRemoved(); }
-    if (id === "sights") { (checked) ? this.props.filteredByTypeSights() : this.props.sightsRemoved(); }
-    if (id === "stands") { (checked) ? this.props.filteredByTypeStands() : this.props.standsRemoved(); }
-    if (id === "armguards") { (checked) ? this.props.filteredByTypeArmguards() : this.props.armguardsRemoved(); }
+    if (id === "risers") {
+      if (checked) {
+        return this.props.filteredByTypeRisers();
+      }
+      return this.props.risersRemoved();
+    }
+    if (id === "limbs") {
+      if (checked) {
+        return this.props.filteredByTypeLimbs();
+      }
+      return this.props.limbsRemoved();
+    }
+    if (id === "finger tabs") {
+      if (checked) {
+        return this.props.filteredByTypeTabs();
+      }
+      return this.props.tabsRemoved();
+    }
+    if (id === "stabilizers") {
+      if (checked) {
+        return this.props.filteredByTypeStabilizers();
+      }
+      return this.props.stabilizersRemoved();
+    }
+    if (id === "arrows") {
+      if (checked) {
+        return this.props.filteredByTypeArrows();
+      }
+      return this.props.arrowsRemoved();
+    }
+    if (id === "sights") {
+      if (checked) {
+        return this.props.filteredByTypeSights();
+      }
+      return this.props.sightsRemoved();
+    }
+    if (id === "stands") {
+      if (checked) {
+        return this.props.filteredByTypeStands();
+      }
+      return this.props.standsRemoved();
+    }
+    if (id === "armguards") {
+      if (checked) {
+        return this.props.filteredByTypeArmguards();
+      }
+      return this.props.armguardsRemoved();
+    }
 
     this.props.setPage1();
-  }
+  };
 
   reset = () => {
     const inputs = document.getElementsByTagName("input");
-    for (var i = 0; i < inputs.length; ++i) {
-      inputs[i].checked = false;
+    for (var input of inputs) {
+      input.checked = false;
     }
     this.props.resetFilters();
-  }
+  };
 
   render() {
-
     const selectData = [
       { id: "PriceAsc", name: "price asc" },
       { id: "PriceDesc", name: "price desc" },
       { id: "NameAsc", name: "name asc" },
       { id: "NameDesc", name: "name desc" },
       { id: "resetFilters", name: "reset filters" }
-    ]
+    ];
 
     const filterData = [
       { id: "risers" },
@@ -61,26 +108,41 @@ class Selector extends React.Component {
       { id: "sights" },
       { id: "stands" },
       { id: "armguards" }
-    ]
+    ];
 
     return (
       <div className="selector-component">
-
         {selectData.map(item => (
           <div key={item.id} className="selector-wrapper">
-            <NavLink  id={item.id} activeClassName="active" style={{ textDecoration: 'none' }} to="/" className="selector-btn menu" onClick={(id) => { this.handleSort(id) }}>{item.name}</NavLink>
-          </div>))}
+            <NavLink
+              id={item.id}
+              activeClassName="active"
+              style={{ textDecoration: "none" }}
+              to="/"
+              className="selector-btn menu"
+              onClick={id => {
+                this.handleSort(id);
+              }}
+            >
+              {item.name}
+            </NavLink>
+          </div>
+        ))}
 
-        <div className='line'></div>
+        <div className="line"></div>
 
         <form className="input-group menu">
           {filterData.map(item => (
             <div key={item.id} className="input-item">
-              <input  id={item.id} type="checkbox" onChange={this.handleFilter} />
+              <input
+                id={item.id}
+                type="checkbox"
+                onChange={this.handleFilter}
+              />
               <label htmlFor={item.id}>{item.id}</label>
-            </div>))}
+            </div>
+          ))}
         </form>
-
       </div>
     );
   }
@@ -89,11 +151,11 @@ class Selector extends React.Component {
 const mapStateToProps = state => {
   return {
     items: state.items
-  }
-}
+  };
+};
 
 const mapDispatchToProps = {
   ...actions
-}
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Selector)
+export default connect(mapStateToProps, mapDispatchToProps)(Selector);
