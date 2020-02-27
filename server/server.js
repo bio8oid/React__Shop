997f23e4a5367f40c12b65b84fe354d3c8a3e3f8
bio8oid <8bollod8@mail.co.uk>
2020-02-27 23:53:03 +0100

    Update server file

--- server/server.js
+++ server/server.js
@@ -1,42 +1,59 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-const express = require('express');
-// const cors = require('cors');
+// const express = require('express');
+// // const cors = require('cors');
 
-const app = express();
+// const app = express();
 
-// app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// // app.use(cors());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-app.get('/', (req, res) => {
-    console.log(res.json(data))
-});
-
+// app.get('/', (req, res) => {
+//     console.log(res.json(data))
+// });
 const uri = "mongodb+srv://8bollod8:bollod@clustershopapp-j4vjy.mongodb.net/Shop_App_Archery?retryWrites=true&w=majority"
 
-MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+MongoClient.connect(uri, function (err, client) {
+    // test.equal(null, err);
+    // test.ok(db != null);
 
-    assert.equal(null, err);
     const db = client.db("Shop_App_Archery");
-    var productsData = db.collection('products').find({});
-    
-    readProducts = doc => {
-        console.log(doc);
-        // let data = [];
-        // data.push(doc)
-        // // console.log(data);
-        // return data;
-    }
-    errorFunc = error => {
-        console.log(error);
-    }
-   const param = productsData.forEach(readProducts, errorFunc);
-    // productsData(x => readProducts);
-    // console.log(readProducts());
-    client.close();
+
+    db.collection("products").find({}, function (err, result) {
+        // test.equal(null, err);
+        // test.equal(1, result);
+
+        db.close();
+        test.done();
+    });
 });
+
+
+
+// MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+
+//     assert.equal(null, err);
+//     const db = client.db("Shop_App_Archery");
+//     var productsData = db.collection('products').find({});
+    
+//     readProducts = doc => {
+//         console.log(doc);
+//         // let data = [];
+//         // data.push(doc)
+//         // // console.log(data);
+//         // return data;
+//     }
+//     errorFunc = error => {
+//         console.log(error);
+//     }
+//    const param = productsData.forEach(readProducts, errorFunc);
+//     // productsData(x => readProducts);
+//     // console.log(readProducts());
+//     // console.log(param);
+//     client.close();
+// });
 
 // let transfer = (readProducts, param) => {
 //     console.log(param)
