b46931d41077cbb67a8feb96e0b509962e69d7fe
bio8oid <8bollod8@mail.co.uk>
2020-04-05 20:33:35 +0200

    Update build

---
+++ build/precache-manifest.d53c53e97d42bf3e604842e9852bc265.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e92c0c9e05fc0e417a98fbdd00997f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62d3e476289a968a74b3",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "1bca54b2aaecc723f7a1",
+    "url": "/static/css/main.080dbe13.chunk.css"
+  },
+  {
+    "revision": "62d3e476289a968a74b3",
+    "url": "/static/js/2.80600215.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.80600215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bca54b2aaecc723f7a1",
+    "url": "/static/js/main.abef660c.chunk.js"
+  },
+  {
+    "revision": "c23971c93c143e87c41a",
+    "url": "/static/js/runtime-main.1de27dd4.js"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "734b583eaa7f95822345db86f966953f",
+    "url": "/static/media/logo-1.734b583e.webp"
+  }
+]);
